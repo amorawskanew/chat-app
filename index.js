468f8d941bf183db1b05c5db5cfa9ea454af2c0c
Anna Morawska <amorawska@yahoo.com>
2020-03-06 15:46:38 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-const app= express()
-const port= 4000
-app.listen (
+const express = require('express')
+const Sse = require('json-sse')
+const cors = require('cors')
+
+const app = express()
+
+const port = process.env.PORT ||  4000
+
+const db = {}
+
+db.messages = []
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+// app.use(cors())
+
+const parser = express.json()
+app.use(parser)
+
+const stream = new Sse()
+
+app.get('/stream', (request, response) => {
+  const action = {
+    type: 'ALL_MESSAGES',
+    payload: db.messages
+  }
+
+  stream.updateInit(action)
+  stream.init(request, response)
+})
+
+app.post(
+  '/message',
+  (request, response) => {
+    const { text } = request.body
+
+    db.messages.push(text)
+
+    response.send(text)
+
+    const action = {
+      type: 'NEW_MESSAGE',
+      payload: text
+    }
+
+    stream.send(action)
+  }
+)
+
+app.listen(
   port,
-  ()=>console.log('listening on :400')
+  () => console.log(`Listening on :${port}`)
 )
